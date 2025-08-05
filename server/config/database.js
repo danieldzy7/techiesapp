ad9eb235079d2c5e624904465ee101b70fd0bc6f
Ryan <ryan@example.com>
2025-08-04 17:19:10 -0700

    Configure app for Render deployment - add render.yaml, update package.json, database config, and add deployment files

--- server/config/database.js
+++ server/config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
       //'url': 'mongodb://' + process.env.IP
-      'url': 'mongodb+srv://gmorningcode:123123q@cluster0.heqaeaj.mongodb.net/techies?retryWrites=true&w=majority&appName=Cluster0'
+      'url': process.env.MONGODB_URI || 'mongodb+srv://gmorningcode:123123q@cluster0.heqaeaj.mongodb.net/techies?retryWrites=true&w=majority&appName=Cluster0'
 }
 
